f2113d64a394734f996a66cbed5544ac8d3727d9
Colin Dorman <csdorman@gmail.com>
2019-05-04 10:40:45 +0000

    non working code inside

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("This is my super-complex app");
 
-console.log("Here is another line of super-neat and awesome code!")
+console.log("Here is another line of super-neat and awesome code!");
+
+cons0le.loog("This a non-working piece of code that will reak things. AHHHH");
